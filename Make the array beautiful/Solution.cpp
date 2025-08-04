dd1c438272848eac03a3682149040a0c55641c12
Khushi Yadav <ky0307005@gmail.com>
2025-08-04 08:44:16 +0000

    Strings Questions

---
+++ Make the array beautiful/Solution.cpp
@@ -0,0 +1,38 @@
+// User function Template for C++
+#include <iostream>
+#include <stack>
+using namespace std;
+
+class Solution {
+  public:
+    vector<int> makeBeautiful(vector<int> arr) {
+        stack<int> s;
+        for(int i = 0; i<arr.size(); i++){
+            if(s.empty()){
+                s.push(arr[i]);
+            } else if (arr[i] >= 0){
+                if(s.top() >= 0){
+                    s.push(arr[i]);
+                } else {
+                    s.pop();
+                }
+            } else {
+                if(s.top() < 0){
+                    s.push(arr[i]);
+                } else {
+                    s.pop();
+                }
+            }
+        }
+        
+        vector<int> ans(s.size());
+        int i = s.size() - 1;
+        
+        while(!s.empty()){
+            ans[i] = s.top();
+            i--;
+            s.pop();
+        }
+        return ans;
+    }
+};
