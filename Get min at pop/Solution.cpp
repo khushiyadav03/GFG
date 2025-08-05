a4b5cbb351cc9feccc0ec48de267edaed4d61e23
Khushi Yadav <ky0307005@gmail.com>
2025-08-05 09:00:03 +0000

    String Questions

---
+++ Get min at pop/Solution.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+
+stack<int> _push(int arr[], int n) {
+    // code here
+    stack<int> st;
+    for(int i = 0; i<n; i++){
+        if(i == 0){
+            st.push(arr[i]);
+        } else {
+            st.push(min(arr[i], st.top()));
+        }
+    }
+    return st;
+}
+
+// Function to print minimum value in stack each time while popping.
+void _getMinAtPop(stack<int> s) {
+    
+    // code here
+    while(!s.empty()){
+        cout << s.top() << " ";
+        s.pop();
+    }
+}
