9b030fba1052809998886665b12ce133744df480
Khushi Yadav <ky0307005@gmail.com>
2025-08-16 11:03:29 +0000

    Stack Questions

---
+++ Next Smaller Element/Solution.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+using namespace std;
+
+vector<int> nextSmallerElement(vector<int> &arr, int n)
+{
+        stack<int> st;
+        vector<int> ans(n, -1);
+
+        for(int i = 0; i < n; i++) {
+            while(!st.empty() && arr[st.top()] > arr[i]) {
+                ans[st.top()] = arr[i];
+                st.pop();
+            }
+            st.push(i);
+        }
+
+        return ans;
+}
