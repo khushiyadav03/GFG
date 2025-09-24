aaea9d3b89f16932be5fbd59b1f75c2a84e51cca
Khushi Yadav <ky0307005@gmail.com>
2025-09-24 14:24:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Allocate Minimum Pages/allocate-minimum-pages.cpp
+++ Difficulty: Medium/Allocate Minimum Pages/allocate-minimum-pages.cpp
@@ -1,41 +1,35 @@
 class Solution {
   public:
-    bool check(vector<int> arr, int max_pages_per_student, int k){
-        int tot=0;
-        int cnt=1;
-        int n=arr.size();
-        for(int i=0;i<n;i++){
-            if(tot+arr[i]>max_pages_per_student){
-                tot=0;
-                tot+=arr[i];
-                cnt++;
-            }
-            else{
-                tot+=arr[i];
-            }
-        }
-        return cnt<=k;
-    }
     int findPages(vector<int> &arr, int k) {
-        int sum=0,maxi=INT_MIN,n=arr.size();
-        if(n<k) return -1;
-        for(int i=0;i<n;i++){
-            maxi=max(maxi,arr[i]);
-            sum+=arr[i];
-        }
-        int i=maxi;
-        int j=sum,mid;
-        while(i<=j){
-            mid=(i+j)/2;
-            if(check(arr,mid, k)){
-                j=mid-1;
-            }
-            else{
-                i=mid+1;
-            }
-        }
-        return i;
-    }
+        int n = arr.size();
+        if (k > n) return -1; // more students than books
 
- 
+        int start = *max_element(arr.begin(), arr.end());
+        int end = accumulate(arr.begin(), arr.end(), 0);
+        int ans = end;
+        
+        while(start <= end){
+            int mid = start + (end - start) / 2;
+            
+            int page = 0, count = 1; // students count
+            
+            for(int i = 0; i < n; i++){
+                if(page + arr[i] > mid){
+                    count++;
+                    page = arr[i];
+                } else {
+                    page += arr[i];
+                }
+            }
+            
+            if(count <= k){
+                ans = mid;
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        
+        return ans;
+    }
 };
