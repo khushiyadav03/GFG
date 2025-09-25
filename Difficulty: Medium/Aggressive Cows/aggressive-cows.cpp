9e4e135b66ef96429e920e02ab9a26c4ed3a45b1
Khushi Yadav <ky0307005@gmail.com>
2025-09-25 14:14:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Aggressive Cows/aggressive-cows.cpp
@@ -0,0 +1,31 @@
+class Solution {
+  public:
+    int aggressiveCows(vector<int> &stalls, int k) {
+        // code here
+        int n = stalls.size();
+        int start = 1, end, mid, ans;
+        sort(stalls.begin(), stalls.end());
+        end = stalls[n - 1] - stalls[0];
+        
+        while(start <= end){
+            mid = start + (end - start) / 2;
+            
+            int count = 1, pos = stalls[0];
+            for(int i = 0; i<n; i++){
+                if(pos + mid <= stalls[i]){
+                    count++;
+                    pos = stalls[i];
+                }
+            }
+            
+            if(count < k){
+                end = mid - 1;
+            } else {
+                ans = mid;
+                start = mid + 1;
+            }
+        }
+        
+        return ans;
+    }
+};
