708cd7cc5117f51a11edc9a884770b15e39e3797
Khushi Yadav <ky0307005@gmail.com>
2025-09-25 16:07:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Search in a sorted Matrix/search-in-a-sorted-matrix.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    bool searchMatrix(vector<vector<int>> &matrix, int x) {
+        // code here
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+        int r = 0, c = cols - 1;
+        
+        while(r < rows && c > -1){
+            int curr = matrix[r][c];
+            
+            if(curr == x){
+                return true;
+            }
+            
+            if(x > curr){
+                r++;
+            } else {
+                c--;
+            }
+        }
+        
+        return false;
+    }
+};
